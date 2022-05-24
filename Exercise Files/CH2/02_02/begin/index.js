2ee9ef81e75f8876e8904b3f4bc46b108abaf4cd
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-05-23 18:47:47 -0700

    adding TensorFlow exercices code

--- Exercise Files/CH2/02_02/begin/index.js
+++ Exercise Files/CH2/02_02/begin/index.js
@@ -2,5 +2,14 @@
 const shape = [4,2];
 // feed data into the tensor
 const data = tf.tensor([4,6,5,9,13,25,1, 57], shape);
+
+// set variables with zeros method
+const data2 = tf.variable(tf.zeros([8]));
+
+
 // print the data
-data.print();
+data2.print();
+
+data2.assign(tf.tensor1d([4,12,5,6,56,3,45,3]));
+
+data2.print();
