f207b0c856860d71888cca8e9addb831dac705cb
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-05-24 11:57:54 -0700

    adding comments for exercices Ch 02, section 03

--- Exercise Files/CH2/02_03/begin/index.js
+++ Exercise Files/CH2/02_03/begin/index.js
@@ -14,3 +14,16 @@ data2.assign(tf.tensor1d([4, 12, 5, 6, 56, 3, 45, 3]));
 data2.print();
 
 
+// create 2 new 1 dimensional tensors
+const data3 = tf.tensor1d([4, 6, 5, 9]);
+const data4 = tf.tensor1d([5, 4, 23, 45]);
+
+// prints
+data3.print();
+data4.print();
+
+
+// adds and multiplies and prints
+data3.add(data4).print();
+data3.mul(data4).print();
+
