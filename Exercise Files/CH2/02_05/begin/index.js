350da37427b2cac4b4a339dd73f5cb0932a6346e
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-05-24 12:14:37 -0700

    adding exercices for Chapter 02, section 05

--- Exercise Files/CH2/02_05/begin/index.js
+++ Exercise Files/CH2/02_05/begin/index.js
@@ -1,20 +1,40 @@
 // define my model
-function simpleAdd(input1, input2) {
+//function simpleAdd(input1, input2) {
     // tidy is used to free up GPU memory once
     // the functions returns
-    return tf.tidy(() => {
-        const x1 = input1;
-        const x2 = input2;
-        const y = x1.add(x2);
-        return y;
-    });
-}
+//    return tf.tidy(() => {
+        //const x1 = input1;
+        //const x2 = input2;
+        //const y = x1.add(x2);
+        //return y;
+    //});
+//}
 
 // new 1 dimensional tensors/arrays
-const data1 = tf.tensor1d([4, 6, 5, 9]);
-const data2 = tf.tensor1d([5, 4, 34, 21]);
+//const data1 = tf.tensor1d([4, 6, 5, 9]);
+//const data2 = tf.tensor1d([5, 4, 34, 21]);
 
 // using the model to do input to output
-const result = simpleAdd(data1,data2);
+//const result = simpleAdd(data1,data2);
 //printing result
-result.print();
+//result.print();
+
+// sequential model
+const model = tf.sequential();
+
+
+model.add(
+    tf.layers.simpleRNN({
+        // only needed first layer
+        inputShape: [20, 4],
+        // the number of units or neurons
+        units: 20, 
+        // weight
+        recurrentInitializer: 'GlorotNormal',
+    })
+);
+
+
+
+
+
